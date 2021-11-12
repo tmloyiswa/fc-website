e7a91e85d267ab60ca907ade655e36b2aeb1951f
Timothy Mloyiswa <tmloyiswa@yahoo.com>
2021-11-12 18:58:40 +0200

    complete large format

--- src/app/large-format/large-format.component.ts
+++ src/app/large-format/large-format.component.ts
@@ -24,28 +24,65 @@ import { Component, OnInit } from '@angular/core';
     <div class="col-4">
     <img src="assets/img/banners.jpeg" class="img-thumbnail" alt="...">
     </div>
-    <div class="col-8">.col-6<br>Subsequent columns continue along the new line.</div>
+    <div class="col-8">
+
+    <p>
+    Have instant impact at any event with <strong>Pull up/ Roll up</strong> banners. They are easily transported and can be erected and lowered in minutes.
+    They are compact and durable and once lowered can be stored with ease. A padded carrier bag comes standard with each banner.
+    </p>
+    <h5>PULL-UP BANNERS</h5>
+    <h6>STANDARD SIZE: 850MM X 2000MM</h6>
+    <li>EXECUTIVE</li>
+    <li>ECONOMY</li>
+    <li>DELUXE</li>
+    <li>High quality full color digital printing.</li>
+
+    </div>
   </div>
 
   <div class="row product">
       <div class="col-4">
       <img src="assets/img/largefc.jpeg" class="img-thumbnail" alt="...">
       </div>
-      <div class="col-8">.col-6<br>Subsequent columns continue along the new line.</div>
+      <div class="col-8">
+
+      <h5>VINYL/PVC PRINTING</h5>
+      <li>Per Square metre</li>
+      <h6>ANY CUSTOMISED SIZES</h6>
+      <li>Custom Wall Banners</li>
+      <li>A0, A1</li>
+      <li>A2, A3</li>
+      <li>A4, A5, ETC</li>
+      <p>High quality full color digital printing.</p>
+      </div>
     </div>
 
     <div class="row product">
         <div class="col-4">
         <img src="assets/img/correx.png" class="img-thumbnail" alt="...">
         </div>
-        <div class="col-8">.col-6<br>Subsequent columns continue along the new line.</div>
+        <div class="col-8">
+        <h5>CORREX BOARDS</h5>
+        <h6>ANY CUSTOMISED SIZES</h6>
+        <li>A0, A1</li>
+        <li>A2, A3</li>
+        <li>A4, A5, ETC</li>
+        <p>For all your indoor & outdoor advertising. High quality full color digital printing.</p>
+        </div>
       </div>
 
       <div class="row product bottom">
           <div class="col-4">
           <img src="assets/img/ABS.png" class="img-thumbnail" alt="...">
           </div>
-          <div class="col-8">.col-6<br>Subsequent columns continue along the new line.</div>
+          <div class="col-8">
+          <h5>CHROMADEC BOARDS</h5>
+          <h6>ANY CUSTOMISED SIZES</h6>
+          <li>A0, A1</li>
+          <li>A2, A3</li>
+          <li>A4, A5, ETC</li>
+          <p>For all your outdoor advertising. High quality full color digital printing.</p>
+          </div>
         </div>
 </div>
   `,
