fa2ec6f403c66acadfc87e1294728aac20b842e8
Timothy Mloyiswa <tmloyiswa@yahoo.com>
2022-06-17 10:41:14 +0200

    fix responsive complete

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   template: `
 <div class="container">
     <div class="row my-custom-lfp justify-content-center">
-        <div class="col-5 p-3 d-flex inside justify-content-center" routerLink="/large-format" style="margin-right: 7px; min-width:49%;">
+        <div class="col-md-5 p-3 d-flex inside justify-content-center" routerLink="/large-format" style="margin-right: 7px; margin-bottom: 10px; min-width:49%;">
                  <figure class="figure text-center">
                        <h5>Large Format Printing</h5>
                        <img src="assets/img/pull-banner.png" class="figure-img img-fluid rounded" alt="...">
@@ -13,7 +13,7 @@ import { Component, OnInit } from '@angular/core';
                        </figure>
                        <span>View More</span>
                  </div>
-        <div class="col-5 p-3 d-flex inside justify-content-center" routerLink="/corporate-branding" style="margin-left: 7px; min-width:49%;">
+        <div class="col-md-5 p-3 d-flex inside justify-content-center" routerLink="/corporate-branding" style="margin-left: 7px; min-width:49%;">
                          <figure class="figure text-center">
                                <h5>Corporate Branding</h5>
                                <img src="assets/img/tshirts.jpg" class="figure-img img-fluid rounded" alt="...">
@@ -24,12 +24,13 @@ import { Component, OnInit } from '@angular/core';
         </div>
     </div>
 
+<div class="container-fluid">
     <div class="row row-middle justify-content-center">
-    <div class="col-2">
+    <div class="col-md-2 p-3 d-flex inside" style="align-items: center; margin-right: 10px; margin-bottom: 10px; justify-content: center;">
      <img id="quick-services" src="assets/img/quickservices.jpg">
      </div>
 
-    <div class="col-3 p-3 d-flex inside justify-content-center" routerLink="/large-format" style="margin-right: 7px; min-width:32%;">
+    <div class="col-md-3 p-3 d-flex inside justify-content-center" routerLink="/promotional-products" style="margin-right: 7px; margin-bottom: 10px; min-width:32%;">
                      <figure class="figure text-center">
                            <h5>Promotional Products</h5>
                            <img src="assets/img/resize flyer.jpg" class="figure-img img-fluid rounded" alt="...">
@@ -38,7 +39,7 @@ import { Component, OnInit } from '@angular/core';
                            <span>View More</span>
                      </div>
 
-<div class="col-3 p-3 d-flex inside justify-content-center" routerLink="/corporate-branding" style="margin-left: 7px; min-width:32%;">
+<div class="col-md-3 p-3 d-flex inside justify-content-center" routerLink="/corporate-stationery" style="margin-left: 7px;  margin-bottom: 10px; min-width:32%;">
                          <figure class="figure text-center">
                                <h5>Corporate Stationery</h5>
                                <img src="assets/img/NCR-Books-removebg-preview.png" class="figure-img img-fluid rounded" alt="...">
@@ -47,90 +48,23 @@ import { Component, OnInit } from '@angular/core';
                                <span>View More</span>
                          </div>
 
-    <div class="col-2"> Col Four </div>
+    <div class="col-md-2 d-flex inside col-fourth" style="align-items: center; margin-left: 10px; flex-direction: column;">
+    <p class="text-center"><strong>Hello and Welcome!!!</strong></p>
 
-</div>
+      <p><strong>Full Custom Pty Ltd </strong></p>
+      <p>The companies’ line of business includes Corporate Branding, Corporate Stationery Printing, Web Applications Design, Printing and Graphic Designing.</p>
+      <p>Our team of employees are highly experienced and friendly...</p>
 
-
-
-<div class="main-container">
-<!--
-<div class="container overflow-hidden">
-  <div class="row row-margin-05">
-    <div class="col d-flex inside align-items-center justify-content-center" routerLink="/large-format">
-      <figure class="figure text-center">
-      <h5>Large Format Printing</h5>
-      <img src="assets/img/pull-banner.png" class="figure-img img-fluid rounded" alt="...">
-        <figcaption class="figure-caption">Pull Up Banners, Viynl Printing & Cut, PVC etc</figcaption>
-      </figure>
-      <span>View More</span>
-    </div>
-    <div class="col">
-    <div class="col d-flex inside align-items-center justify-content-center" routerLink="/corporate-branding">
-      <figure class="figure text-center">
-      <h5>Corporate Branding</h5>
-      <img src="assets/img/tshirts.jpg" class="figure-img img-fluid rounded" alt="...">
-        <figcaption class="figure-caption">T-Shirt Printing, Caps, Mugs, Office Branding etc</figcaption>
-      </figure>
-      <span>View More</span>
-    </div>
-    </div>
+     <p class="great-minds"><strong>Great minds Think alike.</strong></p>
   </div>
 </div>
 
-<div class="container overflow-hidden">
-  <div class="row row-margin-05">
-    <div class="col">
-    <div class="col d-flex inside align-items-center justify-content-center" routerLink="/promotional-products">
-      <figure class="figure text-center">
-      <h5>Promotional Products</h5>
-      <img src="assets/img/resize flyer.jpg" class="figure-img img-fluid rounded" alt="...">
-        <figcaption class="figure-caption">Flyers, Posters, Product Stickers etc</figcaption>
-      </figure>
-      <span>View More</span>
-    </div>
-    </div>
-    <div class="col">
-    <div class="col d-flex inside align-items-center justify-content-center" routerLink="/corporate-stationery">
-      <figure class="figure text-center">
-      <h5>Corporate Stationery</h5>
-      <img src="assets/img/NCR-Books-removebg-preview.png" class="figure-img img-fluid rounded" alt="...">
-        <figcaption class="figure-caption">Invoice Books, Receipt Books, Business Cards etc</figcaption>
-      </figure>
-      <span>View More</span>
-    </div>
-    </div>
-  </div>
 </div>
 
-<div class="container overflow-hidden">
-  <div class="row row-margin-05">
-    <div class="col d-flex">
-    <div class="col d-flex inside align-items-center justify-content-center" routerLink="/vehicle-branding">
-      <figure class="figure text-center">
-      <h5>Vehicle Branding</h5>
-      <img src="assets/img/Car-magnets-1-removebg-preview-1.jpg" class="figure-img car img-fluid rounded" alt="...">
-        <figcaption class="figure-caption">Full Wrap, Magnetic Stickers, Lettering, etc</figcaption>
-      </figure>
-      <span>View More</span>
-    </div>
-    </div>
-    <div class="col">
-    <div class="col d-flex inside align-items-center justify-content-center" routerLink="/toners-refill">
-      <figure class="figure text-center">
-      <h5>Toners & Refill Inks</h5>
-      <img src="assets/img/toners-resize.jpg" class="figure-img img-fluid rounded" alt="...">
-        <figcaption class="figure-caption">Toner Refill Inks and Toner Catridges..</figcaption>
-      </figure>
-      <span>View More</span>
-    </div>
-    </div>
-  </div>
-  -->
 
 <div class="container">
     <div class="row my-custom-lfp justify-content-center">
-        <div class="col-5 p-3 d-flex inside justify-content-center" routerLink="/vehicle-branding" style="margin-right: 7px; min-width:49%;">
+        <div class="col-md-5 p-3 d-flex inside justify-content-center" routerLink="/vehicle-branding" style="margin-right: 7px;  margin-bottom: 10px; min-width:49%;">
                  <figure class="figure text-center">
                        <h5>Vehicle Branding</h5>
                        <img src="assets/img/Car-magnets-1-removebg-preview-1.jpg" class="figure-img car img-fluid rounded" alt="...">
@@ -138,7 +72,7 @@ import { Component, OnInit } from '@angular/core';
                        </figure>
                        <span>View More</span>
                  </div>
-        <div class="col-5 p-3 d-flex inside justify-content-center" routerLink="/toners-refill" style="margin-left: 7px; min-width:49%;">
+        <div class="col-md-5 p-3 d-flex inside justify-content-center" routerLink="/toners-refill" style="margin-left: 7px; min-width:49%;">
                          <figure class="figure text-center">
                                <h5>Toners & Refill Inks</h5>
                                <img src="assets/img/toners-resize.jpg" class="figure-img img-fluid rounded" alt="...">
@@ -156,9 +90,8 @@ import { Component, OnInit } from '@angular/core';
             margin-top: 0.5em;
             margin-bottom: 0.5em;
 }
+
 img#quick-services {
-position: absolute;
-margin-left: 10%;
 height:280px !important;
 }
 
@@ -166,7 +99,6 @@ height:280px !important;
     margin-left: 80px;
     margin-right: 80px;
 }
-
 
              img {
                   height: 216px !important;
@@ -177,11 +109,6 @@ height:280px !important;
         .row {
                    background: lightblue;
                    padding: 1rem !important;
-        }
-
-        .row-margin-05 {
-        margin-top: 0.5em;
-        margin-bottom: 0.5em;
         }
 
         img.car {
@@ -229,6 +156,7 @@ height:280px !important;
         display: flex;
 max-width: 12%;
         }
+
   `
   ]
 })
